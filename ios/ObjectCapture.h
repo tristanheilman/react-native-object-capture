56931f1902f476d1cb603344a753f411a1cf8e31
Tristan Heilman <tristanheilman@outlook.com>
2025-05-24 16:16:21 -0400

    chore: initial commit

---
+++ ios/ObjectCapture.h
@@ -0,0 +1,5 @@
+#import <ObjectCaptureSpec/ObjectCaptureSpec.h>
+
+@interface ObjectCapture : NSObject <NativeObjectCaptureSpec>
+
+@end
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import ObjectCapture from './NativeObjectCapture';
+
+export function multiply(a: number, b: number): number {
+  return ObjectCapture.multiply(a, b);
+}
